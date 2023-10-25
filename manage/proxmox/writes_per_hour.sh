a7b0dcfedd1f2e7c2a8b443ae4479fc8765bb3fd
Rahul Sharma <rahulsrma26@gmail.com>
2023-10-25 21:22:57 +0100

    :bug: fixed calculations

--- manage/proxmox/writes_per_hour.sh
+++ manage/proxmox/writes_per_hour.sh
@@ -1,6 +1,13 @@
-SECONDS=$(uptime | grep -Eo "[0-9]+:[0-9]+:[0-9]+" | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
-UNITS_WRITTEN=$(smartctl /dev/nvme0n1 --all | grep -Eo "Data Units Written:\s+[0-9,]+" | sed -e $'s/[^0-9]//g')
-echo "$UNITS_WRITTEN data units written in $SECONDS seconds"
-#result=$(($UNITS_WRITTEN / $SECONDS))
-result=`echo "scale=4; $UNITS_WRITTEN / ($SECONDS / 3600)" | bc`
-echo "$result units/hour"
+smartctl /dev/nvme0n1 --all | grep -E "Data Units Written:\s+[0-9,]+"
+
+START=$(tail -n 1 .ssd_write_history |  tr ',' '\n' | head -n 1)
+NOW=$(date '+%m/%d/%Y %H:%M:%S')
+END=$(date --date "$NOW" +%s)
+SECONDS=`echo "($END - $START)" | bc`
+echo "Uptime:                             $SECONDS seconds [ts: $START]"
+
+UNITS_START=$(tail -n 1 .ssd_write_history |  tr ',' '\n' | tail -n 1)
+UNITS_NOW=$(smartctl /dev/nvme0n1 --all | grep -Eo "Data Units Written:\s+[0-9,]+" | sed -e $'s/[^0-9]//g')
+UNIT_RATE=`echo "scale=4; ($UNITS_NOW - $UNITS_START) / ($SECONDS / 3600)" | bc`
+DATA_RATE=`echo "scale=4; (($UNITS_NOW - $UNITS_START)/2) / ($SECONDS / 3600)" | bc`
+echo "Burn rate:                          $DATA_RATE MB/hour [$UNIT_RATE units/hour]"
