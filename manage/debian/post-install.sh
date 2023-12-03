919605cd8a31884ba0be222df0f4204c7a23567d
Rahul Sharma <rahulsrma26@gmail.com>
2023-12-03 21:12:08 +0000

    :bug: fixed sed not writing

--- manage/debian/post-install.sh
+++ manage/debian/post-install.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 apt update
 apt install sudo -y
-adduser admin
-usermod -aG sudo admin
+
+read -p "Enter sudo username (e.g. admin): " USERNAME
+adduser ${USERNAME}
+usermod -aG sudo ${USERNAME}
 
 read -p "Enter ip/subnet (e.g. 192.168.0.10/24): " ADDRESS
 read -p "Enter gateway (e.g. 192.168.0.1): " GATEWAY
 
-DATA="static\\n\\taddress ${ADDRESS}"
-sed "s/dhcp/${DATA}/" /etc/network/interfaces
+DATA="static\\n\\taddress ${ADDRESS}\\n\\tgateway ${GATEWAY}\\n"
+sed -i -e "s/dhcp/${DATA}/" /etc/network/interfaces
 systemctl restart networking
